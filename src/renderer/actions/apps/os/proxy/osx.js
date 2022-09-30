3c230f52a569f10cbd462d73d768d3af75581560
nsrCodes <navdeepsinghrathore2000@gmail.com>
2022-09-30 21:54:27 +0530

    RQLY-440 fix: read and don't overwrite existing bypass list

--- src/renderer/actions/apps/os/proxy/osx.js
+++ src/renderer/actions/apps/os/proxy/osx.js
@@ -1,9 +1,12 @@
+import { promisify } from "util";
+import * as _ from "lodash";
 const { exec } = require("child_process");
+const promisifiedExec = promisify(exec)
 
 let proxyNames = ["http_proxy", "HTTP_PROXY", "https_proxy", "HTTPS_PROXY"];
 const networkServices = ["Wi-Fi"];
 let desktopProxyNames = ["webproxy", "securewebproxy"];
-const bypassDomains = ["meet.google.com"]
+const RQBypassDomains = ["meet.google.com"]
 
 const proxyCommands = {
   SET_DESKTOP: "SET_DESKTOP",
@@ -14,24 +17,53 @@ const proxyCommands = {
   UNSET_BYPASS: "UNSET_BYPASS",
 };
 
+/* MAINTAINING BYPASS LIST */
+// @nsr move to separate file when providing this as a feature
+let originalBypassDomains = {}
+let bypassDomains = {}
+
+networkServices.forEach(async serviceName => {
+  bypassDomains[serviceName] = await getBypassDomainsList(serviceName);
+})
+
+async function getBypassDomainsList(serviceName) {
+  let cmdOutput = await promisifiedExec(`networksetup -getproxybypassdomains ${serviceName};`)
+  if (
+    !cmdOutput.stderr &&
+    !cmdOutput.stdout.includes("There aren't any bypass domains set") >=0
+  ) {
+    originalBypassDomains[serviceName] = cmdOutput.stdout.trim().split("\n")
+  }
+
+  return _.union(originalBypassDomains[serviceName], RQBypassDomains);
+}
+
+const getBypassString = (service) => {
+  return bypassDomains[service] ? bypassDomains[service].join(" ") : "";
+}
+const getOriginalBypassString = (service) => {
+  return originalBypassDomains[service]?.length ? originalBypassDomains[service].join(" ") : "Empty";
+}
+
 // TODO fix/rewrite:
 // this function just returns the command of commandType
 // but it updates all the other strings too, very inefficient
-const getExecutionCommand = (
+function getExecutionCommand (
   commandType,
   HOST,
   PORT,
   proxyName,
   serviceName
-) => {
-  const bypassList = bypassDomains.join(" ");
+) {
+  const bypassString = getBypassString(serviceName)
+  const originalBypassString = getOriginalBypassString(serviceName)
   const commandTemplates = {
     SET_DESKTOP: `networksetup -set${proxyName} ${serviceName} ${HOST} ${PORT};`,
     UNSET_DESKTOP: `networksetup -set${proxyName}state ${serviceName} off;`,
     SET_TERMINAL: `export ${proxyName}=${HOST}:${PORT};`,
     UNSET_TERMINAL: `unset ${proxyName};`,
-    SET_BYPASS: `networksetup -setproxybypassdomains ${serviceName} ${bypassList};`,
-    UNSET_BYPASS: `networksetup -setproxybypassdomains ${serviceName} Empty;`
+    SET_BYPASS: `networksetup -setproxybypassdomains ${serviceName} ${bypassString};`,
+    UNSET_BYPASS: `networksetup -setproxybypassdomains ${serviceName} ${originalBypassString};`
   };
 
   return commandTemplates[commandType];
