568f69d41cf122eed7daaf31ff48de36bea28af0
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2023-02-19 20:33:47 +0530

    RQLY-1153 fix: don't rebuild win-version-info (#24)

--- .erb/scripts/electron-rebuild.js
+++ .erb/scripts/electron-rebuild.js
@@ -1,15 +1,22 @@
-import path from "path";
 import { execSync } from "child_process";
-import fs from "fs";
+import fs, {readdirSync} from "fs";
 import { dependencies } from "../../release/app/package.json";
 import webpackPaths from "../configs/webpack.paths";
+
+const getModules = (source, exclude) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory() && !exclude.includes(dirent.name))
+    .map(dirent => dirent.name);
 
 if (
   Object.keys(dependencies || {}).length > 0 &&
   fs.existsSync(webpackPaths.appNodeModulesPath)
 ) {
-  const electronRebuildCmd =
-    "../../node_modules/.bin/electron-rebuild --parallel --force --types prod,dev,optional --module-dir .";
+    // Skip modules that are already prebuilt
+    const exclude = ['win-version-info'];
+    const modules = getModules(webpackPaths.appNodeModulesPath, exclude);
+
+    const electronRebuildCmd = `../../node_modules/.bin/electron-rebuild --parallel --types prod,dev,optional --only ${modules.toString()} --module-dir .`;
   const cmd =
     process.platform === "win32"
       ? electronRebuildCmd.replace(/\//g, "\\")
