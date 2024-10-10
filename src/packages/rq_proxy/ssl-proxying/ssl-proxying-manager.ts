2ff49d7782f75a0151f3ffdaa4dcc5ea298d2f26
nsrCodes <navdeepsinghrathore2000@gmail.com>
2024-10-10 23:22:23 +0530

    chore: cleanup and package updates

--- src/packages/rq_proxy/ssl-proxying/ssl-proxying-manager.ts
+++ src/packages/rq_proxy/ssl-proxying/ssl-proxying-manager.ts
@@ -1,86 +1,86 @@
-import { ISource, SSLProxyingJsonObj } from "lib/storage/types/ssl-proxying";
-import BaseConfigFetcher from "renderer/lib/proxy-interface/base";
-// TODO: @sahil fix this by adding type.d.ts file
-//@ts-ignore
-import { RULE_PROCESSOR } from "@requestly/requestly-core";
+// import { ISource, SSLProxyingJsonObj } from "lib/storage/types/ssl-proxying";
+// import BaseConfigFetcher from "renderer/lib/proxy-interface/base";
+// // TODO: @sahil fix this by adding type.d.ts file
+// //@ts-ignore
+// import { RULE_PROCESSOR } from "@requestly/requestly-core";
 
-class SSLProxyingManager {
-  configFetcher: BaseConfigFetcher;
+// class SSLProxyingManager {
+//   configFetcher: BaseConfigFetcher;
 
-  constructor(configFetcher: BaseConfigFetcher) {
-    this.configFetcher = configFetcher;
-  }
+//   constructor(configFetcher: BaseConfigFetcher) {
+//     this.configFetcher = configFetcher;
+//   }
 
-  isSslProxyingActive = (urlOrigin: string): boolean => {
-    const config: SSLProxyingJsonObj = this.configFetcher.getConfig();
+//   isSslProxyingActive = (urlOrigin: string): boolean => {
+//     const config: SSLProxyingJsonObj = this.configFetcher.getConfig();
 
-    if (config.enabledAll === false) {
-      const inclusionListSuccess: boolean = this.checkStatusWithInclusionList(
-        config,
-        urlOrigin
-      );
-      if (inclusionListSuccess) {
-        console.log(`${urlOrigin} inclusion List`);
-        return true;
-      }
+//     if (config.enabledAll === false) {
+//       const inclusionListSuccess: boolean = this.checkStatusWithInclusionList(
+//         config,
+//         urlOrigin
+//       );
+//       if (inclusionListSuccess) {
+//         console.log(`${urlOrigin} inclusion List`);
+//         return true;
+//       }
 
-      return false;
-    } else {
-      const exclusionListSuccess: boolean = this.checkStatusWithExclusionList(
-        config,
-        urlOrigin
-      );
-      if (exclusionListSuccess) {
-        console.log(`${urlOrigin} exclusion List`);
-        return false;
-      }
+//       return false;
+//     } else {
+//       const exclusionListSuccess: boolean = this.checkStatusWithExclusionList(
+//         config,
+//         urlOrigin
+//       );
+//       if (exclusionListSuccess) {
+//         console.log(`${urlOrigin} exclusion List`);
+//         return false;
+//       }
 
-      return true;
-    }
-  };
+//       return true;
+//     }
+//   };
 
-  checkStatusWithInclusionList = (
-    config: SSLProxyingJsonObj,
-    urlOrigin: string
-  ): boolean => {
-    const inclusionListSources: ISource[] = Object.values(
-      config.inclusionList || {}
-    );
-    return this.checkStatusWithList(inclusionListSources, urlOrigin);
-  };
+//   checkStatusWithInclusionList = (
+//     config: SSLProxyingJsonObj,
+//     urlOrigin: string
+//   ): boolean => {
+//     const inclusionListSources: ISource[] = Object.values(
+//       config.inclusionList || {}
+//     );
+//     return this.checkStatusWithList(inclusionListSources, urlOrigin);
+//   };
 
-  checkStatusWithExclusionList = (
-    config: SSLProxyingJsonObj,
-    urlOrigin: string
-  ): boolean => {
-    const exclusionListSources: ISource[] = Object.values(
-      config.exclusionList || {}
-    );
-    return this.checkStatusWithList(exclusionListSources, urlOrigin);
-  };
+//   checkStatusWithExclusionList = (
+//     config: SSLProxyingJsonObj,
+//     urlOrigin: string
+//   ): boolean => {
+//     const exclusionListSources: ISource[] = Object.values(
+//       config.exclusionList || {}
+//     );
+//     return this.checkStatusWithList(exclusionListSources, urlOrigin);
+//   };
 
-  checkStatusWithList = (
-    sourceObjs: ISource[] = [],
-    urlOrigin: string = ""
-  ): boolean => {
-    return sourceObjs.some((sourceObj) =>
-      this.checkStatusForSource(sourceObj, urlOrigin)
-    );
-  };
+//   checkStatusWithList = (
+//     sourceObjs: ISource[] = [],
+//     urlOrigin: string = ""
+//   ): boolean => {
+//     return sourceObjs.some((sourceObj) =>
+//       this.checkStatusForSource(sourceObj, urlOrigin)
+//     );
+//   };
 
-  checkStatusForSource = (
-    sourceObject: ISource,
-    urlOrigin: string
-  ): boolean => {
-    const result = RULE_PROCESSOR.RuleMatcher.matchUrlWithRuleSource(
-      sourceObject,
-      urlOrigin
-    );
-    if (result === "") {
-      return true;
-    }
-    return false;
-  };
-}
+//   checkStatusForSource = (
+//     sourceObject: ISource,
+//     urlOrigin: string
+//   ): boolean => {
+//     const result = RULE_PROCESSOR.RuleMatcher.matchUrlWithRuleSource(
+//       sourceObject,
+//       urlOrigin
+//     );
+//     if (result === "") {
+//       return true;
+//     }
+//     return false;
+//   };
+// }
 
-export default SSLProxyingManager;
+// export default SSLProxyingManager;
