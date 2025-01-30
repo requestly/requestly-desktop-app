255d320ff11cf0968da1ddb56c4bf84bdbc0a84d
Navdeep Singh Rathore <57226514+nsrCodes@users.noreply.github.com>
2025-01-30 17:09:09 +0530

    feat: blocklist (#126)
    * feat: blocklist
    
    * fix
    
    ---------
    
    Co-authored-by: Sahil Gupta <sahil865gupta@gmail.com>

--- src/renderer/actions/apps/browsers/chromium-based-browsers.js
+++ src/renderer/actions/apps/browsers/chromium-based-browsers.js
@@ -39,7 +39,16 @@ const getChromiumLaunchOptions = async (
     if (value === undefined || value === null) return `--${key}`;
     return `--${key}=${value}`;
   });
-
+  const blockList = await ipcRenderer
+    .invoke("get-storage-object", "blocked_domains")
+    .then((blockedDomains) => {
+      console.log("Blocked domains", blockedDomains);
+      return blockedDomains;
+    })
+    .catch((err) => {
+      console.error(err);
+      return [];
+    });
   return {
     browser,
     proxy: `http://${staticConfig.PROXY_HOST}:${proxyPort}`,
@@ -47,6 +56,7 @@ const getChromiumLaunchOptions = async (
       // Force even localhost requests to go through the proxy
       // See https://bugs.chromium.org/p/chromium/issues/detail?id=899126#c17
       "<-loopback>",
+      ...(blockList || []),
       // Don't intercept our warning hiding requests. Note that this must be
       // the 2nd rule here, or <-loopback> would override it.
       // hideWarningServer.host,
