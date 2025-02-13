7fb12345191274f1bec1bca0ab42efbd8a1b05c3
Nafees Nehar <nafees87n@gmail.com>
2025-02-13 20:49:57 +0530

    [ENGG-2547] propagate error from API client requester

--- src/main/actions/makeApiClientRequest.js
+++ src/main/actions/makeApiClientRequest.js
@@ -75,8 +75,9 @@ const makeApiClientRequest = async ({ apiRequest }) => {
       redirectedUrl: responseURL !== url ? responseURL : "",
     };
   } catch (e) {
-    console.log("Error while making api client request", e);
-    return null;
+    return {
+      error: e.message,
+    };
   }
 };
 
